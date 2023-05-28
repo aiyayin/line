65814e1835b767ffdc56bb479692ccc4579e0592
fuyi <fuyi@doutu.tv>
2023-05-28 17:55:30 +0800

    [demo][new]文字滚动

--- demo/src/main/java/com/yin/lin/demo/html/ArticleActivity.java
+++ demo/src/main/java/com/yin/lin/demo/html/ArticleActivity.java
@@ -22,8 +22,9 @@ public class ArticleActivity extends BaseActivity {
         TextView text = findViewById(R.id.text);
         text.setText(HtmlCompat.fromHtml(convertStreamToString(R.raw.article3), HtmlCompat.FROM_HTML_OPTION_USE_CSS_COLORS));
 //        RichText.fromHtml(R.raw.article).into(text);
-
-
+        MarqueeView marqueeView = findViewById(R.id.mMarqueeView);
+        marqueeView.setText("===看着怎么和v老姑父；pi'upu【噗噗片===");
+        marqueeView.startAnim();
 //过滤图片标签
 //        Pattern compile = Pattern.compile("<\\s*(img|IMG)\\s+([^>]*)\\s*>");
 //        String[] strings = compile.split(content);
